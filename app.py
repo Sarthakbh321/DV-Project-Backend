b3db7f0aed240e24b0118a96964fa74f2fcc6bc9
Sarthak <sarthakbh321@gmail.com>
2021-06-03 20:20:02 +0530

    added POST request

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import base64
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from codebase.gender_wise import GenderWisePlacementAnalysis
 import PIL
 from codebase.subject_wise import SubjectWisePlacementAnalysis
+from codebase.bar_chart import generateBarChart
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/')
@@ -26,6 +31,21 @@ def get_graph():
 
     return jsonify(res)
 
+@app.route('/generate', methods=["POST"])
+def generate():
+    print("HI")
+    if(request.method == "POST"):
+        params = request.json['params']
+
+        b64 = generateBarChart(params)
+
+        res = {
+            "image": b64
+        }
+
+        return jsonify(res)
+
+    return "NOT GET"
 
 
 if __name__ == '__main__':
---
+++ codebase/bar_chart.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import pandas
+import base64
+import io
+
+def generateBarChart(params):
+    column_list = ["sl_no", "gender", "ssc_p", "ssc_b", "hsc_p", "hsc_b", "hsc_s", "degree_p", "degree_t", "workexp",
+                   "etest_p", "specialisation", "mba_p", "status", "salary"]
+
+    df = pandas.read_csv("codebase/datasets/dataset.csv", usecols=column_list)
+
+    print(params["filter"]["gender"])
+    if(params["filter"]["gender"] != "both"):
+        df = df[(df["gender"] == params["filter"]["gender"])]
+
+    print(len(df))
+    x = df[params["x_axis"]]
+    y = df[params["y_axis"]]
+
+    plt.clf()
+    plt.xlabel(params["x_axis"])
+    plt.ylabel(params["y_axis"])
+
+    xticks = [i for i in range(1, len(x) + 1)]
+    yticks = [i for i in range(1, len(y) + 1)]
+
+    # plt.xticks(xticks, x, rotation='vertical')
+    # plt.yticks(yticks, y)
+    plt.title("Generated Chart")
+
+    plt.bar(x, y)
+
+    bytes = io.BytesIO()
+
+    plt.savefig(bytes, format="jpg")
+    bytes.seek(0)
+
+    b64 = base64.b64encode(bytes.read()).decode("ascii")
+
+    return b64
+
+
+if __name__ == '__main__':
+    params = {"x_axis": "sl_no", "y_axis": "salary"}
+    generateBarChart(params)
+
+
+
+
+
